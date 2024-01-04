b5132d569b6d715d1b5e43eaa3e9a6543df8903e
Itohan1 <momoduitohan5@gmail.com>
2024-01-04 01:44:33 -0800

    only uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        tem = i
+        if ord(tem) >= 97 and ord(tem) <= 122:
+            tem = chr(ord(i) - 32)
+        print(f"{tem}", end='')
+    print()
+
