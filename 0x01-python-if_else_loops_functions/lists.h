18118eb1d39c0873215d31eb590a2bd82a244a91
Itohan1 <momoduitohan5@gmail.com>
2024-01-04 23:45:20 -0800

    main.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,4 +2,10 @@
 #define LISTS_H
 
 def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
 #endif
