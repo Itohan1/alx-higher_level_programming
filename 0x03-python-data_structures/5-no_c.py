4ee430810be21ebc013abea92e1ef314fc51660e
Itohan1 <momoduitohan5@gmail.com>
2024-01-08 07:37:58 -0800

    c is an exception

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     if my_string[:]:
         new_string = my_string.translate({ord("c"): None})
-        Another_string = new_string.translate({ord("c"): None})
+        Another_string = new_string.translate({ord("C"): None})
         return Another_string
     return my_string
