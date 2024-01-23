164cc95f88b868a87fbe246622b640dc13ee85f9
Itohan1 <momoduitohan5@gmail.com>
2024-01-23 08:03:27 -0800

    all

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,8 +19,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """defines a square by: (based on 5-square.py)"""
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -35,8 +33,6 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """defines a square by: (based on 5-square.py)"""
-
         if (not isinstance(value, tuple)) or len(value) != 2 or not all(isinstance(n, int) and n >= 0 for n in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
