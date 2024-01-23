4e75644d1400ca0c45ec63315b93a80cbd757153
Itohan1 <momoduitohan5@gmail.com>
2024-01-23 07:33:01 -0800

    changes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,6 +51,7 @@ class Square:
 
         if self.__size == 0:
             print("")
+            return
 
         [print("") for a in range(self.__position[1])]
         for i in range(0, self.__size):
