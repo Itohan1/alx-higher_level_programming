f59bc0a5e830236a396bb2a35ac1f2bbe56144dc
Itohan1 <momoduitohan5@gmail.com>
2024-01-25 04:42:42 -0800

    first

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a function that adds 2 integers"""
+
+def add_integer(a, b=98):
+    """a and b must be integers or floats.
+
+    Raise a TypeError exception with the message a must be an integer or b must be an integer
+
+    a and b must be first casted to integers if they are float
+
+    Returns an integer: the addition of a and b"""
+
+    if ((not isinstance(a, int)) and (not isinstance(a, float))):
+        raise TypeError("a must be an integer or b must be an integer")
+    if ((not isinstance(b, int)) and (not isinstance(a, float))):
+        raise TypeError("a must be an integer or b must be an integer")
+    return(int(a) + int(b))
