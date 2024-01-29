c8db3e42335d07a7b16d652e579c939ded758ddf
Itohan1 <momoduitohan5@gmail.com>
2024-01-29 07:24:00 -0800

    changes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)"""
+    """a class Rectangle that defines a rectangle"""
 
     number_of_instances = 0
     print_symbol = '#'
@@ -19,7 +19,7 @@ class Rectangle:
     def width(self):
         """Private instance attribute: width"""
 
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -34,7 +34,7 @@ class Rectangle:
     def height(self):
         """Private instance attribute: height"""
 
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
@@ -67,15 +67,15 @@ class Rectangle:
             [rec.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rec.append("\n")
-        return("".join(rec))
+        return ("".join(rec))
 
     def __repr(self):
-        """return a string representation of the rectangle to be able to recreate a new instance by using eval()"""
+        """return a string representation of the rectangle"""
 
-        return f"Rectangle({self.__width}, {self.__height})"
+        return (f"Rectangle({self.__width}, {self.__height})")
 
     def __del__(self):
-        """Print the message Bye rectangle... when an instance of Rectangle is deleted"""
+        """Print the message Bye rectangle..."""
 
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
@@ -92,6 +92,6 @@ class Rectangle:
         area_2 = rect_2.area()
 
         if area_1 >= area_2:
-            return(rect_1)
+            return (rect_1)
         else:
-            return(rect_2)
+            return (rect_2)
