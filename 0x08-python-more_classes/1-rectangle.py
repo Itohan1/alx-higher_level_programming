0fbcb53a3dedc37e7490c02044bab8998fe9889a
Itohan1 <momoduitohan5@gmail.com>
2024-01-29 05:31:57 -0800

    changes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,23 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Instantiation with optional width and height"""
 
-        self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """Private instance attribute: width"""
-
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        self.width = width
 
     @property
     def height(self):
@@ -35,8 +20,23 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TYpeError("height must be an integer")
+            raise TypeError("height must be an integer")
 
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        """Private instance attribute: width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TYpeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
