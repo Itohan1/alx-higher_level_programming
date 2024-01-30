0d476bcd6cef25eb6cf695390a1b04edb8a45740
Itohan1 <momoduitohan5@gmail.com>
2024-01-30 06:29:16 -0800

    change cycled linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,13 +14,13 @@ int check_cycle(listint_t *list)
 	while (first != NULL && second != NULL && second->next != NULL)
 	{
 		first = first->next;
-		second = second->next;
+		second = second->next->next;
 
 		if (first == second)
 		{
 			return (1);
 		}
-	}
 
+	}
 	return (0);
 }
