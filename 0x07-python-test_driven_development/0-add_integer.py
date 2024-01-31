11380f774718b5f2e2771da18798cfeeb63c4d13
Itohan1 <momoduitohan5@gmail.com>
2024-01-31 03:53:13 -0800

    tests files and functions

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """Write a function that adds 2 integers"""
 
+
 def add_integer(a, b=98):
-    """a and b must be integers or floats.
-
-    Raise a TypeError exception with the message a must be an integer or b must be an integer
-
-    a and b must be first casted to integers if they are float
-
-    Returns an integer: the addition of a and b"""
+    """a and b must be integers or floats"""
 
     if ((not isinstance(a, int)) and (not isinstance(a, float))):
         raise TypeError("a must be an integer or b must be an integer")
     if ((not isinstance(b, int)) and (not isinstance(a, float))):
         raise TypeError("a must be an integer or b must be an integer")
-    return(int(a) + int(b))
+    return (int(a) + int(b))
