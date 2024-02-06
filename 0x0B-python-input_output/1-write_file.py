ac09cf7394de2625f6f961aa8f3572f83c6417eb
Itohan1 <momoduitohan5@gmail.com>
2024-02-06 06:53:06 -0800

    write the file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""writes a string to a text file (UTF8)"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)"""
+    with open(filename, "w",  encoding="utf-8") as dfile:
+        return (dfile.write(text))
