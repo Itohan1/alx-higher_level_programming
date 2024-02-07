3eeb8be8003b6673bf5e3e42a16c17fa55b43881
Itohan1 <momoduitohan5@gmail.com>
2024-02-07 02:21:35 -0800

    changes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,6 +3,7 @@
 
 
 class Student:
+    """a class Student that defines a student"""
     def __init__(self, first_name, last_name, age):
         """a class Student that defines a student"""
         self.first_name = first_name
