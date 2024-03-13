50fceca8495056ef7c52d0aefbf89ea46124d142
Itohan1 <momoduitohan5@gmail.com>
2024-03-13 05:28:06 -0700

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -2,4 +2,9 @@
 -- Write a script that creates the database
 
 DROP DATABASE IF EXISTS hbtn_oc_0;
-CREATE DATABASE IF NOT EXISTS hbtn_oc_0;
+CREATE DATABASE hbtn_oc_0;
+Use hbtn_oc_0;
+CREATE TABLE datanames (
+	names VARCHAR(500)
+);
+
