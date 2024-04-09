efe3a0b4715adbd7ca1634c7cf59f3e3ebdd1a53
Itohan1 <momoduitohan5@gmail.com>
2024-04-08 17:43:49 -0700

    multitype

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-multiType = [
-	'C is fun',
-	'Python is cool',
-	'JavaScript is amazing'
+const multiType = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
-for (let j of multiType) {
-	console.log(j);
+for (const j of multiType) {
+  console.log(j);
 }
