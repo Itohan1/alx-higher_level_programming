05868c297fc6a121bd3cf92d842214043637082d
Itohan1 <momoduitohan5@gmail.com>
2024-04-08 20:18:28 -0700

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,16 +3,16 @@
 const { argv } = require('node:process');
 
 function factorial (n) {
-	if (n === 0) {
-		return (1);
-	}
+  if (n === 0) {
+    return (1);
+  }
 
-	return (n * factorial(n - 1));
+  return (n * factorial(n - 1));
 }
 
 if (!argv[2]) {
-	console.log(1);
+  console.log(1);
 } else {
-	const result = factorial(parseInt(argv[2]));
-	console.log(result);
+  const result = factorial(parseInt(argv[2]));
+  console.log(result);
 }
