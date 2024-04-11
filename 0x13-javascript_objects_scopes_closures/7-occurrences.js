d10d57612d25c7fc9aa1f242217d70bfbd79e818
Itohan1 <momoduitohan5@gmail.com>
2024-04-10 17:13:33 -0700

    list in list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (let i = 0; i < list.lenght; i++) {
+  for (const i in list) {
     if (list[i] === searchElement) {
       count++;
     }
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const reversedlist = []
+  const reversedlist = [];
 
   for (let i = list.length; i >= 0; i--) {
-    if (ifnNaN) {
-	    consol.log('String');
-
+    if (list[i] === undefined) {
+    }
     reversedlist.push(list[i]);
   }
   return (reversedlist);
-}
+};
