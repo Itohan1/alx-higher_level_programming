f01aa8a26adbd8a47013cdd7f81623c3eaf70524
Itohan <momoduitohan5@gmail.com>
2024-04-13 18:25:44 +0100

    sql connectioon

---
+++ x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    curr = conn.cursor()
+    curr.execute(f"SELECT * FROM {sys.argv[3]}.states ORDER BY states.id ASC")
+    q_rows = curr.fetchall()
+
+    for rows in q_rows:
+        print(rows)
+
+    curr.close()
+    conn.close()
