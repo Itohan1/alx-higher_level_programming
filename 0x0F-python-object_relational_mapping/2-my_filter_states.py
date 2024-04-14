02611e004894603a9ae8c81d36124cf5e4b9affd
Itohan <momoduitohan5@gmail.com>
2024-04-14 19:45:28 +0100

    where name is qrgument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    curr = conn.cursor()
+    curr.execute(
+        """SELECT * FROM {}.states
+        WHERE states.name='{}'
+        ORDER BY states.id ASC"""
+        .format(sys.argv[3], sys.argv[4])
+    )
+    q_rows = curr.fetchall()
+
+    for rows in q_rows:
+        print(rows)
+
+    curr.close()
+    conn.close()
