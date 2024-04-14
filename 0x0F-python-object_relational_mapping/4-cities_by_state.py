3341d5fe0ac645edc51097291c55d3feec496014
Itohan <momoduitohan5@gmail.com>
2024-04-14 22:46:05 +0100

    Write a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Write a script that lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+
+
+def con_injection(user, passwd, db):
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db,
+        charset="utf8"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute(
+        f"""SELECT cities.id, cities.name, states.name
+                FROM cities INNER JOIN
+                states ON cities.state_id = states.id
+                ORDER BY cities.id ASC"""
+    )
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    arg = sys.argv
+    con_injection(arg[1], arg[2], arg[3])
