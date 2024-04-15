82b171e5eb4089fc37ea30b2f075edbda5dbdf04
Itohan <momoduitohan5@gmail.com>
2024-04-15 20:47:31 +0100

    number 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,13 +17,15 @@ def con_injection(user, passwd, db):
 
     cur = conn.cursor()
 
-    cur.execute(
-        f"""SELECT cities.name
+    query = """SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC"""
-    )
+
+    search = arg[4]
+
+    cur.execute((query), (search,))
 
     rows = cur.fetchall()
 
