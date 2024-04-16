15ac08a189d8deb5c3e4147b45ac4745dfdae680
Itohan <momoduitohan5@gmail.com>
2024-04-16 21:15:28 +0100

    11 model

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects from ithe database"""
+"""Write a script that prints the first State object"""
+
 
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     engine = create_engine(
             """mysql+mysqldb://{}:{}@localhost/{}"""
             .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    new_state = State(name='louisiana', id=6)
+    session.add(new_state)
+    state = session.commit()
+    new_state_id = new_state.id
+    print(new_state.id)
+
     session.close()
