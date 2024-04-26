d212b549e78ac7479e7ac5d2598ee9d2cda7574b
itohan1 <momoduitohan5@gmail>
2024-04-26 07:04:15 -0700

    post params

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL as an argument
-response=$(curl -s -H "X-School-User-Id: 98" "$1"); echo -n "Hello School!"; if [ -z "$response" ]; then exit 1; fi
+value="98"; curl -s -H "X-School-User-Id=$value" "$1"; echo -n "Hello School!" ; if [ -z ]
