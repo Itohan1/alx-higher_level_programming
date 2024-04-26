c30c5018aae9e421a18a9dca63b20d7643babfff
itohan1 <momoduitohan5@gmail>
2024-04-26 04:29:20 -0700

    Hello school

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL as an argument
-curl -s -H "X-School-User-Id: 98" "$1" ; echo "Hello School!"
+curl -s -H "X-School-User-Id: 98" "$1" ; echo -n "Hello School!"
