105abb8442ea7abfd7e38f7146f79772f8dfb548
itohan1 <momoduitohan5@gmail>
2024-04-27 06:11:04 -0700

    content type json

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL as an argument
-value="98"; curl -s -H "X-School-User-Id=$value" "$1"; echo -n "Hello School!" ; if [ -z ]
+curl -s -H "X-School-User-Id: 98" "$1"
