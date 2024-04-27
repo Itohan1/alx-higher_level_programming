4fca97226e3940c729161642b508d685be27fd82
itohan1 <momoduitohan5@gmail>
2024-04-27 08:06:36 -0700

    the new file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+        print("Body_response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- content:", html.decode('utf-8'))
