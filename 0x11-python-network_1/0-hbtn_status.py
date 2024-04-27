716ee792ed91adc65d4b596dac252cde3df6bc4b
itohan1 <momoduitohan5@gmail>
2024-04-27 08:13:50 -0700

    the new file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import urllib.request
 """Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
-
-
+import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
