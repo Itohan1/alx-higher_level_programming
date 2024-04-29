41d873c7562987a63ca6c183444a48dc11ee90f4
itohan1 <momoduitohan5@gmail>
2024-04-29 08:10:32 -0700

    Write a Python script that takes in a URL and an email address

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,8 +6,9 @@ import urllib.parse
 
 if __name__ == "__main__":
     """Write a Python script that takes in a URL and an email"""
+
     data = urllib.parse.urlencode({'email': sys.argv[2]})
-    req = urllib.request.Request(sys.argv[1], data, method='POST')
+    req = urllib.request.Request(sys.argv[1], data, method="POST")
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
-        print("You email")
+        print(f"Your email is: {body}")
