7c5a19a53bb92483789742a782fb24234a282ab1
itohan1 <momoduitohan5@gmail>
2024-04-29 01:14:06 -0700

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.parse
 if __name__ == "__main__":
     """Write a Python script that takes in a URL and an email"""
     data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
-    req = urllib.request.Request(sys.argv[1], data, method='POST')
+    req = urllib.request.Request(sys.argv[1], data=data, method='POST')
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
         print('Your email is: ', body)
