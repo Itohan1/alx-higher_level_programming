6f447574edb37fbcec49bc4e29c0ea222a08bf28
itohan1 <momoduitohan5@gmail>
2024-04-29 08:11:53 -0700

    Write a Python script that takes in a URL and an email address

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     """Write a Python script that takes in a URL and an email"""
     req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     if req.status_code == 200:
-        print(f"Your email is: {req.status_code}")
+        print(f"Your email is: {req.text}")
