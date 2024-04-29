688a53e1f9186a6c34e37083af717b97359f0dd6
itohan1 <momoduitohan5@gmail>
2024-04-29 01:07:42 -0700

    email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    """Write a Python script that takes in a URL and an email"""
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print('Your email is:', body)
