ab17c7a653770fc6eea415c601895b14855c6b14
Itohan <momoduitohan5@gmail.com>
2024-05-22 11:30:16 +0100

    first task

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file
+
+const fs = require('fs');
+const name = process.argv[2];
+
+fs.readFile(name, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
