716a4ad42b0626471454261c037a2a679f5bf277
Itohan <momoduitohan5@gmail.com>
2024-05-23 15:14:33 +0100

    request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//Write a script that gets the contents of a webpage and stores it in a file
+// Write a script that gets the contents of a webpage and stores it in a file
 
 const request = require('request');
 const fs = require('fs');
@@ -8,7 +8,7 @@ const name = process.argv[2];
 request(name, (err, response, body) => {
   if (err) console.error(err);
 
-  fs.writeFile('loripsum', body, err => {
+  fs.writeFile('loripsum', body, (err) => {
     if (err) console.error(err);
   });
 });
