2e88d3e053350f74d780894218a47c5d32e3eebb
Itohan <momoduitohan5@gmail.com>
2024-05-23 12:06:25 +0100

    films people

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,18 +9,16 @@ request(url, { json: true }, (error, response, body) => {
   if (error) console.log(error);
   const vare = body.results;
 
-  const elementKey = vare.find(element => typeof element === 'object' && 'characters' in element);
-  if (elementKey) {
-    for (const i of elementKey.characters) {
-      const words = i.split('e/');
-      if (words[1] === '18/') {
-        const moreWords = 'e/';
-        const newWords = words[0].concat(moreWords, words[1]);
-        request(newWords, { json: true }, (error, anodaResponse, anodabody) => {
-          if (error) console.log(error);
-          console.log(anodabody.films.length);
-        });
+  let count = 0;
+  for (const i of vare) {
+    if (i.characters) {
+      for (const j of i.characters) {
+        const words = j.split('e/');
+        if (words[1] === '18/') {
+          count++;
+        }
       }
     }
   }
+  console.log(count);
 });
